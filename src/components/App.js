af206de99c5c20bc9754950f4684cf6d5a1f79b3
Chandru <137380053+VKIDS2004@users.noreply.github.com>
2023-09-23 21:59:40 +0530

    loading

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Dayorder from "./dayorder";
 import "../app.scss";
 import Colortheme from "./Colortheme";
 import axios from "axios";
+import { api } from "./api";
 
 function App() {
   const [step, setStep] = useState(0);
@@ -17,13 +18,10 @@ function App() {
     const getApi = async () => {
         
     try{
-        const res = await axios.get(
-            "https://6508523156db83a34d9c20cf.mockapi.io/api/chandru"
-        );
-           
-        await axios.put("https://6508523156db83a34d9c20cf.mockapi.io/api/chandru/" + 1, {
-            clicks : res.data[0].clicks+1
-        });
+        const res = await axios.get("https://6508523156db83a34d9c20cf.mockapi.io/api/chandru");
+        await axios.put("https://6508523156db83a34d9c20cf.mockapi.io/api/chandru/" + 1, {clicks : res.data[0].clicks+1});
+        await axios.put("https://6508523156db83a34d9c20cf.mockapi.io/api/chandru/" + 1, {date : {name:"dfk"}} );
+
         setclicks(()=>res.data[0].clicks+1);
     }
     catch(ex){
@@ -31,27 +29,21 @@ function App() {
     }
     };
     getApi();
+    // api();
   }, []);
 
 
   return (
     <div className="App">
-      {/* <BrowserRouter>
-        <Routes>
-        {/* <Route path="/" element={}> 
-            
-          </Route>
-      </Routes>
-      
-    </BrowserRouter> */}
-
       <Header />
       <DateTime step={step} setStep={setStep} color={color} />
       <Dayorder step={step} />
       <Colortheme color={color} setcolor={setcolor} />
-      <p className="copyright">Copyright @ 2023 Chandru ( Viewers : {clicks} )</p>
+      <p className="copyright">Copyright @ 2023 Chandru (  {clicks?`viewers : ${clicks}`:"loading...."} )</p>
     </div>
   );
 }
+
+
 
 export default App;
